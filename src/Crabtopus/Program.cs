4937aaf12df0ec193cfc371d9fa2913a47d83ca5
Keuvain <k94ll13nn3@outlook.fr>
2019-03-27 23:27:35 +0100

    Rework parsing of blobs

--- src/Crabtopus/Program.cs
+++ src/Crabtopus/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Crabtopus
 {
@@ -9,24 +11,26 @@ namespace Crabtopus
     {
         public static void Main(string[] args)
         {
-            string content = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"Low\Wizards Of The Coast\MTGA\output_log.txt");
-            List<Blob> blobs = FindBlobs(content);
-            foreach (Blob blob in blobs)
-            {
-                Console.WriteLine(blob.Name);
-            }
+            ReadOnlySpan<char> content = File
+                .ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"Low\Wizards Of The Coast\MTGA\output_log.txt")
+                .AsSpan();
+            List<Blob> blobs = FindBlobs(content.ToString());
+
+            // Sample
+            Blob blob = blobs.Find(x => x.Name.StartsWith("PlayerInventory.GetPlayerInventory"));
+            Console.WriteLine(JObject.Parse(ReadBlob(blob, content)).ToString(Formatting.Indented));
         }
 
         private static List<Blob> FindBlobs(string content)
         {
             var blobs = new List<Blob>();
-            MatchCollection matches = Regex.Matches(content, @"(?:(\w*\.\w*\([\d]+\))[\r\n]*{.*?})+", RegexOptions.Singleline);
+            MatchCollection matches = Regex.Matches(content, @"(?:<== (\w*\.\w*\([\d]+\))[\r\n]*{)+", RegexOptions.Singleline);
             foreach (Match item in matches)
             {
                 blobs.Add(new Blob(item.Groups[1].Value, false));
             }
 
-            matches = Regex.Matches(content, @"(?:(\w*\.\w*\([\d]+\))[\r\n]*\[.*?}[\r\n]*])+", RegexOptions.Singleline);
+            matches = Regex.Matches(content, @"(?:<== (\w*\.\w*\([\d]+\))[\r\n]*{)+", RegexOptions.Singleline);
             foreach (Match item in matches)
             {
                 blobs.Add(new Blob(item.Groups[1].Value, true));
@@ -35,25 +39,44 @@ namespace Crabtopus
             return blobs;
         }
 
-        private static string ReadBlob(Blob blob, string content)
+        private static string ReadBlob(Blob blob, in ReadOnlySpan<char> content)
         {
-            Match match;
-            string suffix;
-            string prefix;
+            char start;
+            char end;
+            int countStart = 1;
+            int countEnd = 0;
+
             if (blob.IsArray)
             {
-                match = Regex.Match(content, Regex.Escape(blob.Name) + @"[\r\n]*\[(.*?)}[\r\n]*]", RegexOptions.Singleline);
-                suffix = "}]";
-                prefix = "[";
+                start = '[';
+                end = ']';
             }
             else
             {
-                match = Regex.Match(content, Regex.Escape(blob.Name) + @"[\r\n]*{(.*?)}", RegexOptions.Singleline);
-                suffix = "}";
-                prefix = "{";
+                start = '{';
+                end = '}';
             }
 
-            return match.Success ? Regex.Replace(prefix + match.Groups[1].Value + suffix, @"\t|\n|\r", "") : null;
+            int indexOfBlob = content.IndexOf($"<== {blob.Name}".AsSpan());
+            int indexOfStart = content.Slice(indexOfBlob).IndexOf(start);
+            ReadOnlySpan<char> sliceToSearch = content.Slice(indexOfBlob + indexOfStart + 1);
+            int index = 0;
+            while (countStart != countEnd)
+            {
+                if (sliceToSearch[index] == start)
+                {
+                    countStart++;
+                }
+
+                if (sliceToSearch[index] == end)
+                {
+                    countEnd++;
+                }
+
+                index++;
+            }
+
+            return start + sliceToSearch.Slice(0, index).ToString();
         }
     }
 }
