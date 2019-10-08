b7b539aa66c4e526d00e46a4cd298bf52d546146
Keuvain <k94ll13nn3@outlook.fr>
2019-10-08 20:09:48 +0200

    Clean current code

--- src/Crabtopus/Program.cs
+++ src/Crabtopus/Program.cs
@@ -5,22 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using AngleSharp;
+using AngleSharp.Dom;
 
 namespace Crabtopus
 {
     public static class Program
     {
-        private const string _baseUrl = "www.mtgtop8.com";
+        private const string BaseUrl = "www.mtgtop8.com";
 
-        private static IBrowsingContext _context;
+        private static IBrowsingContext Context;
 
         public static async Task Main()
         {
             bool sideboardLine = false;
-            _context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
-            var events = await GetEvents();
-            var decks = await GetDecks(events.First().id);
-            foreach (var (count, title, sideboard) in await GetDeck(events.First().id, decks.First().id))
+            Context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+            IEnumerable<(string title, int id)> events = await GetEventsAsync();
+            foreach ((string title, int id) in events)
+            {
+                Console.WriteLine($"{id} {title}");
+            }
+
+            Console.WriteLine("---------------------------------------");
+            IEnumerable<(string title, int id)> decks = await GetDecksAsync(events.First().id);
+            foreach ((string title, int id) in decks)
+            {
+                Console.WriteLine($"{id} {title}");
+            }
+
+            Console.WriteLine("---------------------------------------");
+            foreach ((int count, string title, bool sideboard) in await GetDeckAsync(events.First().id, decks.First().id))
             {
                 if (sideboard && !sideboardLine)
                 {
@@ -32,37 +45,37 @@ namespace Crabtopus
             }
         }
 
-        private static async Task<IEnumerable<(string title, int id)>> GetEvents()
+        private static async Task<IEnumerable<(string title, int id)>> GetEventsAsync()
         {
-            string address = $"https://{_baseUrl}/format?f=ST";
-            var document = await _context.OpenAsync(address);
+            string address = $"https://{BaseUrl}/format?f=ST";
+            IDocument document = await Context.OpenAsync(address);
             const string cellSelector = ".page > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(3) > tbody:nth-child(1) > tr > td:nth-child(1) > a:nth-child(1)";
-            var cells = document.QuerySelectorAll(cellSelector);
-            return cells.Select(m => (title: m.TextContent, id: int.Parse(HttpUtility.ParseQueryString(new Uri($"https://{_baseUrl}/{m.GetAttribute("href")}").Query)["e"], CultureInfo.InvariantCulture)));
+            IHtmlCollection<IElement> cells = document.QuerySelectorAll(cellSelector);
+            return cells.Select(m => (title: m.TextContent, id: int.Parse(HttpUtility.ParseQueryString(new Uri($"https://{BaseUrl}/{m.GetAttribute("href")}").Query)["e"], CultureInfo.InvariantCulture)));
         }
 
-        private static async Task<IEnumerable<(string title, int id)>> GetDecks(int eventId)
+        private static async Task<IEnumerable<(string title, int id)>> GetDecksAsync(int eventId)
         {
-            string address = $"https://{_baseUrl}/event?e={eventId}&f=ST";
-            var document = await _context.OpenAsync(address);
+            string address = $"https://{BaseUrl}/event?e={eventId}&f=ST";
+            IDocument document = await Context.OpenAsync(address);
             const string cellSelector = "div.chosen_tr > div:nth-child(2) > a:nth-child(1), div.hover_tr > div:nth-child(2) > a:nth-child(1)";
-            var cells = document.QuerySelectorAll(cellSelector);
-            return cells.Select(m => (title: m.TextContent, id: int.Parse(HttpUtility.ParseQueryString(new Uri($"https://{_baseUrl}/{m.GetAttribute("href")}").Query)["d"], CultureInfo.InvariantCulture)));
+            IHtmlCollection<IElement> cells = document.QuerySelectorAll(cellSelector);
+            return cells.Select(m => (title: m.TextContent, id: int.Parse(HttpUtility.ParseQueryString(new Uri($"https://{BaseUrl}/{m.GetAttribute("href")}").Query)["d"], CultureInfo.InvariantCulture)));
         }
 
-        private static async Task<IEnumerable<(int count, string title, bool sideboard)>> GetDeck(int eventId, int deckId)
+        private static async Task<IEnumerable<(int count, string title, bool sideboard)>> GetDeckAsync(int eventId, int deckId)
         {
-            string address = $"https://{_baseUrl}/event?e={eventId}&d={deckId}&f=ST";
-            var document = await _context.OpenAsync(address);
+            string address = $"https://{BaseUrl}/event?e={eventId}&d={deckId}&f=ST";
+            IDocument document = await Context.OpenAsync(address);
             const string mainDeckSelector = "table.Stable:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:not(:last-child) > table > tbody > tr > td > div:nth-child(1)";
             const string sideboardSelector = "table.Stable:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:last-child > table > tbody > tr > td > div:nth-child(1)";
-            var mainDeckCells = document.QuerySelectorAll(mainDeckSelector);
-            var sideboardCells = document.QuerySelectorAll(sideboardSelector);
+            IHtmlCollection<IElement> mainDeckCells = document.QuerySelectorAll(mainDeckSelector);
+            IHtmlCollection<IElement> sideboardCells = document.QuerySelectorAll(sideboardSelector);
 
             const string nameSelector = ".S16";
-            var nameCells = document.QuerySelectorAll(nameSelector).Select(m => m.TextContent).First();
+            string nameCells = document.QuerySelectorAll(nameSelector).Select(m => m.TextContent).First();
 
-            System.Console.WriteLine(nameCells);
+            Console.WriteLine(nameCells);
 
             return mainDeckCells.Select(m => (count: (int)char.GetNumericValue(m.TextContent[0]), title: m.TextContent[1..].Trim(), sideboard: false))
                 .Concat(sideboardCells.Select(m => (count: (int)char.GetNumericValue(m.TextContent[0]), title: m.TextContent[1..].Trim(), sideboard: true)));
