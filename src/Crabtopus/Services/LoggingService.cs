9fd671a58fc16e4b4575e2fb7b05c1d58c0a1149
Keuvain <k94ll13nn3@outlook.fr>
2019-10-12 18:53:13 +0200

    Update LoggingService

--- src/Crabtopus/Services/LoggingService.cs
+++ src/Crabtopus/Services/LoggingService.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Globalization;
-using System.IO;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -10,16 +8,10 @@ namespace Crabtopus.App.Services
 {
     public class LoggingService
     {
-        private readonly string _logDirectory;
-        private readonly string _logFile;
-
         public LoggingService(DiscordSocketClient discord, CommandService commands)
         {
             _ = discord ?? throw new ArgumentNullException(nameof(discord));
             _ = commands ?? throw new ArgumentNullException(nameof(commands));
-
-            _logDirectory = Path.Combine(AppContext.BaseDirectory, "logs");
-            _logFile = Path.Combine(_logDirectory, $"{DateTime.UtcNow.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}.txt");
 
             discord.Log += OnLogAsync;
             commands.Log += OnLogAsync;
@@ -27,20 +19,7 @@ namespace Crabtopus.App.Services
 
         private Task OnLogAsync(LogMessage msg)
         {
-            if (!Directory.Exists(_logDirectory))
-            {
-                Directory.CreateDirectory(_logDirectory);
-            }
-
-            if (!File.Exists(_logFile))
-            {
-                File.Create(_logFile).Dispose();
-            }
-
-            string logText = $"{DateTime.UtcNow.ToString("hh:mm:ss", CultureInfo.InvariantCulture)} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}";
-            File.AppendAllText(_logFile, $"{logText}{Environment.NewLine}");
-
-            return Console.Out.WriteLineAsync(logText);
+            return Console.Out.WriteLineAsync($"{DateTime.Now.ToShortTimeString()} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}");
         }
     }
 }
