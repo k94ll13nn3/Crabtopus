418fe744a452644009606c7a8a4a6dcc982b2d83
Keuvain <k94ll13nn3@outlook.fr>
2020-04-14 13:45:49 +0200

    Start to add more mana symbols

---
+++ src/Crabtopus/Converters/CostToManaConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Crabtopus.Converters
+{
+    [ValueConversion(typeof(string), typeof(List<Viewbox>))]
+    public class CostToManaConverter : IValueConverter
+    {
+        public static readonly CostToManaConverter Default = new CostToManaConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boxes = new List<Viewbox>();
+            if (value is string s)
+            {
+                foreach (string manaCost in s.Split('o', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    if (manaCost == "G")
+                    {
+                        boxes.Add(Application.Current.FindResource("MonoGreen") as Viewbox);
+                    }
+
+                    if (manaCost == "R")
+                    {
+                        boxes.Add(Application.Current.FindResource("MonoRed") as Viewbox);
+                    }
+                }
+            }
+
+            return boxes;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
