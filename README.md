e21f47be07399e8379cc334582277810ba28be7e
Kévin Gallienne <k94ll13nn3@outlook.fr>
2020-04-18 16:19:03 +0200

    Add info about app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Crabtopus
 
 Magic: The Gathering Arena tool
+
+Crabtopus is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.
--- src/Crabtopus/ViewModels/NotifyIconViewModel.cs
+++ src/Crabtopus/ViewModels/NotifyIconViewModel.cs
@@ -1,10 +1,35 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Crabtopus.ViewModels
 {
     internal class NotifyIconViewModel : ViewModelBase
     {
-        public static ICommand ExitApplicationCommand => new DelegateCommand(() => Application.Current.Shutdown());
+        public NotifyIconViewModel()
+        {
+            ExitApplicationCommand = new DelegateCommand(() => Application.Current.Shutdown());
+            Version = $"v{FileVersionInfo.GetVersionInfo(Assembly.GetAssembly(typeof(NotifyIconViewModel))?.Location).ProductVersion}";
+            About = $@"Crabtopus {Version}
+by @k94ll13nn3
+
+Crabtopus is unofficial Fan Content permitted under the 
+Fan Content Policy. Not approved/endorsed by Wizards. 
+Portions of the materials used are property of Wizards of the Coast. 
+©Wizards of the Coast LLC.
+
+Used packages:
+- Hardcodet.NotifyIcon.Wpf
+- AngleSharp
+- Humanizer
+- EFCore.BulkExtensions";
+        }
+
+        public ICommand ExitApplicationCommand { get; set; }
+
+        public string Version { get; set; }
+
+        public string About { get; set; }
     }
 }
